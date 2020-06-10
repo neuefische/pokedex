9d1f018da7ce2caf8de572cbabe9250e626dd7a0
macfrei <lene.frei@neuefische.de>
2020-06-10 18:09:09 +0200

    Sytling and functionality in pokedex.js

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -1,12 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./Pokedex.css";
 
 export default function Pokedex() {
+  const [pkmList, setPkmList] = useState([]);
+  const [pkmDetail, setPkmDetail] = useState(false);
+
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=100")
+      .then((response) => response.json())
+      .then((data) => {
+        setPkmList(data.results);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  }, []);
+
+  function handleClick(pokemon) {
+    fetch(`${pokemon.url}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setPkmDetail(data);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  }
   return (
-    <div className="Pokedex-container">
-      <ul className="Pokedex-list">
-        <li>Pokemon</li>
+    <div className="Pokedex--container">
+      <ul className="Pokedex--list">
+        {pkmList.map((pokemon) => (
+          <li key={pokemon.name}>
+            <button
+              className="Pokedex--list__button active"
+              onClick={() => handleClick(pokemon)}
+            >
+              {pokemon.name}
+            </button>
+          </li>
+        ))}
       </ul>
-      <div className="Pokedex-detail">Here is a Pokemon</div>
+
+      {pkmDetail ? (
+        <div className="Pokedex--detail">
+          <span>{pkmDetail.name}</span>
+          <img alt={pkmDetail.name} src={pkmDetail.sprites.front_default} />
+        </div>
+      ) : (
+        <div className="Pokedex--detail">No Pokemon</div>
+      )}
     </div>
   );
 }
